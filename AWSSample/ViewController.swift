7ed18a717ef8519f3f1106f82e3a85ff2e018c39
Awais Shahid <awaiss@theentertainerasia.com>
2021-01-14 15:18:22 +0500

    Initial Commit

---
+++ AWSSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWSSample
+//
+//  Created by Awais Shahid on 14/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
